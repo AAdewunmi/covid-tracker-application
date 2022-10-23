fac839fed2089d0159629487990322cc1e204167
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-23 12:06:20 +0100

    Initial Project Commit

---
+++ src/test/java/com/application/covidtrackerapplication/CovidTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.covidtrackerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
