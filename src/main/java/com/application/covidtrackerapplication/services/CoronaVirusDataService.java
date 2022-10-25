e29403874ddb7e489e78c79b80e29774f5fbea27
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-25 11:00:47 +0100

    Fourth Commit: Implement getAllStats() in CoronaVirusDataService Class.

--- src/main/java/com/application/covidtrackerapplication/services/CoronaVirusDataService.java
+++ src/main/java/com/application/covidtrackerapplication/services/CoronaVirusDataService.java
@@ -1,8 +1,11 @@
 package com.application.covidtrackerapplication.services;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CoronaVirusDataService {
     private static String VIRUS_DATA_URL =
             "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
