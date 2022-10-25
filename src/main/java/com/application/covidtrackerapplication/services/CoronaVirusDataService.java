cd873c69ef6c5b0e7b1508fdf52435ec7c36a66c
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-25 10:29:29 +0100

    Third Commit: Create variables for CoronaVirusDataService Class.

--- src/main/java/com/application/covidtrackerapplication/services/CoronaVirusDataService.java
+++ src/main/java/com/application/covidtrackerapplication/services/CoronaVirusDataService.java
@@ -1,4 +1,13 @@
 package com.application.covidtrackerapplication.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CoronaVirusDataService {
+    private static String VIRUS_DATA_URL =
+            "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    private List<LocationStats> allStats = new ArrayList<>();
+
+
+
 }
