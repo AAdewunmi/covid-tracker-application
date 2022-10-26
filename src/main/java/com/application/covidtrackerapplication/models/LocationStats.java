9efdd8ee17a1912975bb2b181e72eaaf0074911f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-26 16:38:35 +0100

    Tenth Commit: Implement getters and setters for LocationStats Class.

--- src/main/java/com/application/covidtrackerapplication/models/LocationStats.java
+++ src/main/java/com/application/covidtrackerapplication/models/LocationStats.java
@@ -7,5 +7,35 @@ public class LocationStats {
     private int latestTotalCases;
     private int diffFromPrevDay;
 
+    public String getState() {
+        return state;
+    }
 
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getLatestTotalCases() {
+        return latestTotalCases;
+    }
+
+    public void setLatestTotalCases(int latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
+
+    public int getDiffFromPrevDay() {
+        return diffFromPrevDay;
+    }
+
+    public void setDiffFromPrevDay(int diffFromPrevDay) {
+        this.diffFromPrevDay = diffFromPrevDay;
+    }
 }
