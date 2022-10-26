d35c15fe78f5490813985d7b5c20475689461613
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-26 16:36:48 +0100

    Ninth Commit: Declare Variables for LocationStats Class.

--- src/main/java/com/application/covidtrackerapplication/models/LocationStats.java
+++ src/main/java/com/application/covidtrackerapplication/models/LocationStats.java
@@ -1,4 +1,11 @@
 package com.application.covidtrackerapplication.models;
 
 public class LocationStats {
+
+    private String state;
+    private String country;
+    private int latestTotalCases;
+    private int diffFromPrevDay;
+
+
 }
