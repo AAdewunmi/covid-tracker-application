a676e440d9334de27d9fd0a7b507d10ab2765098
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-27 13:34:13 +0100

    Fifteenth Commit: Add @Autowired annotation to Implement access to CoronaVirusDataService Class from HomeController Class.

--- src/main/java/com/application/covidtrackerapplication/controllers/HomeController.java
+++ src/main/java/com/application/covidtrackerapplication/controllers/HomeController.java
@@ -1,11 +1,13 @@
 package com.application.covidtrackerapplication.controllers;
 
 import com.application.covidtrackerapplication.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class HomeController {
 
+    @Autowired
     CoronaVirusDataService coronaVirusDataService;
 
 }
