5386bda558fb62d00c2c2629d49786da939d1585
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-27 13:32:32 +0100

    Fourteenth Commit: Implement access to CoronaVirusDataService Class from HomeController Class.

--- src/main/java/com/application/covidtrackerapplication/controllers/HomeController.java
+++ src/main/java/com/application/covidtrackerapplication/controllers/HomeController.java
@@ -1,7 +1,11 @@
 package com.application.covidtrackerapplication.controllers;
 
+import com.application.covidtrackerapplication.services.CoronaVirusDataService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class HomeController {
+
+    CoronaVirusDataService coronaVirusDataService;
+
 }
