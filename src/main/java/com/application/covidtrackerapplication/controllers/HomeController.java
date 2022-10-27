3c356ce39d1546bcb82d086be3437391ed223dbe
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-27 13:51:40 +0100

    Sixteen Commit: Implement home() method in HomeController Class.

--- src/main/java/com/application/covidtrackerapplication/controllers/HomeController.java
+++ src/main/java/com/application/covidtrackerapplication/controllers/HomeController.java
@@ -1,13 +1,28 @@
 package com.application.covidtrackerapplication.controllers;
 
+import com.application.covidtrackerapplication.models.LocationStats;
 import com.application.covidtrackerapplication.services.CoronaVirusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @Autowired
     CoronaVirusDataService coronaVirusDataService;
+
+
+    public String home(Model model){
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+        return "home";
+    }
 
 }
