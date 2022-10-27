d79836bffc28c709e70ed661a686966e017b8c46
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-27 13:54:04 +0100

    Seventeenth Commit: Add @GetMapping annotation to home() method in HomeController Class.

--- src/main/java/com/application/covidtrackerapplication/controllers/HomeController.java
+++ src/main/java/com/application/covidtrackerapplication/controllers/HomeController.java
@@ -5,6 +5,7 @@ import com.application.covidtrackerapplication.services.CoronaVirusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
@@ -14,7 +15,7 @@ public class HomeController {
     @Autowired
     CoronaVirusDataService coronaVirusDataService;
 
-
+    @GetMapping("/")
     public String home(Model model){
         List<LocationStats> allStats = coronaVirusDataService.getAllStats();
         int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
